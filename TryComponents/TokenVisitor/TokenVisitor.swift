ded9e6491f6ae245f3f40664017a2c8d5e0b22b6
OnagaHaruki <haloupgarou@gmail.com>
2022-10-30 23:11:57 +0900

    プロパティを宣言した。

--- TryComponents/TokenVisitor/TokenVisitor.swift
+++ TryComponents/TokenVisitor/TokenVisitor.swift
@@ -9,9 +9,21 @@ import Foundation
 import SwiftSyntax
 
 final class TokenVisitor: SyntaxRewriter {
+    private var currentStruct: StructHolder?
+    private var currentClass: ClassHolder?
+    private var currentEnum: EnumHolder?
+    private var currentTypeName: TypeName?
+    private var getedTypes =  [String: Any]()
+    
+    private enum TypeName {
+        case `struct`
+        case `class`
+        case `enum`
+        case `protocol`
+    }
     
     override func visitPre(_ node: Syntax) {
-        
+        let syntaxNodeType = "\(node.syntaxNodeType)"
     }
     
     override func visit(_ token: TokenSyntax) -> Syntax {
