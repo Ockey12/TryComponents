ceab19e545b02de314183bd5f45ae6078e221a61
OnagaHaruki <haloupgarou@gmail.com>
2022-10-31 00:23:05 +0900

    型の宣言を見つけたときのvisitPre()とvisitPost()の処理を追加した。ただし、currentを初期化するだけで、内容は抽出できていない。

--- TryComponents/TokenVisitor/TokenVisitor.swift
+++ TryComponents/TokenVisitor/TokenVisitor.swift
@@ -12,8 +12,8 @@ final class TokenVisitor: SyntaxRewriter {
     private var currentStruct: StructHolder?
     private var currentClass: ClassHolder?
     private var currentEnum: EnumHolder?
-    private var currentTypeName: TypeName?
-    private var getedTypes =  [String: Any]()
+    private var currentTypeFlag: TypeName?
+    var getedTypes =  [String: Any]()
     
     private enum TypeName {
         case `struct`
@@ -23,7 +23,38 @@ final class TokenVisitor: SyntaxRewriter {
     }
     
     override func visitPre(_ node: Syntax) {
-        let syntaxNodeType = "\(node.syntaxNodeType)"
+        let currentSyntaxNodeType = "\(node.syntaxNodeType)"
+        var parentSyntaxNodeType = ""
+        if let parent = node.parent?.syntaxNodeType {
+            parentSyntaxNodeType = "\(parent)"
+        }
+        
+        // Struct/Class/Enum/ProtocolDeclSyntaxのとき
+        // currentStruct/Class/Enumを初期化する
+        // currentTypeFlagを、宣言している型に更新する
+        switch currentSyntaxNodeType {
+        case SyntaxNodeType.structDeclSyntax:
+            if parentSyntaxNodeType == SyntaxNodeType.codeBlockItemSyntax {
+                currentStruct = StructHolder()
+                currentTypeFlag = .struct
+            }
+        case SyntaxNodeType.classDeclSyntax:
+            if parentSyntaxNodeType == SyntaxNodeType.codeBlockItemSyntax {
+                currentClass = ClassHolder()
+                currentTypeFlag = .class
+            }
+        case SyntaxNodeType.enumDeclSyntax:
+            if parentSyntaxNodeType == SyntaxNodeType.codeBlockItemSyntax {
+                currentEnum = EnumHolder()
+                currentTypeFlag = .enum
+            }
+        case SyntaxNodeType.protocolDeclSyntax:
+            if parentSyntaxNodeType == SyntaxNodeType.codeBlockItemSyntax {
+                currentTypeFlag = .protocol
+            }
+        default:
+            break
+        }
     }
     
     override func visit(_ token: TokenSyntax) -> Syntax {
@@ -31,6 +62,34 @@ final class TokenVisitor: SyntaxRewriter {
     }
     
     override func visitPost(_ node: Syntax) {
+        let currentSyntaxNodeType = "\(node.syntaxNodeType)"
         
+        // CodeBlockItemSyntaxのとき
+        // 型の宣言が終わる
+        // currentStruct/Class/EnumをgetedTypesに格納する
+        // currentStruct/Class/Enumにnilを代入して初期状態に戻す
+        if currentSyntaxNodeType == SyntaxNodeType.codeBlockItemSyntax {
+            switch currentTypeFlag {
+            case .struct:
+                if let structHolder = currentStruct {
+                    getedTypes["\(structHolder.name)"] = ["\(structHolder.name)": structHolder]
+                }
+                currentStruct = nil
+            case .class:
+                if let classHolder = currentClass {
+                    getedTypes["\(classHolder.name)"] = ["\(classHolder.name)": classHolder]
+                }
+                currentClass = nil
+            case .enum:
+                if let enumHolder = currentEnum {
+                    getedTypes["\(enumHolder.name)"] = ["\(enumHolder.name)": enumHolder]
+                }
+                currentEnum = nil
+            case .protocol:
+                break
+            default:
+                break
+            }
+        }
     }
 }
